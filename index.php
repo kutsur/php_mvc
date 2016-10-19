34e27ec37e94988aaff1b0e8bff2155b5f1f3078
Nikita Kutsur <nikita_kutsur@art-fox.ru>
2016-10-19 23:29:20 +0300

    Creating First Files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Test Page for ITSM</title>
+        <meta charset="utf-8" />
+        <link href="./css/style.css" rel="stylesheet" media="all">
+    </head>
+    <body>
+    <header>
+    </header>
+    </body>
+</html>
