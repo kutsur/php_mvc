08f6c248eae591b15595f8ba741d52402207b06c
Nikita Kutsur <nikita_kutsur@art-fox.ru>
2016-10-23 20:58:26 +0300

    add new string base

--- app/PostsModel.php
+++ app/PostsModel.php
@@ -1,6 +1,16 @@
 <?php
 	require_once "DataBase.php";
 
+	class Post{
 
-
+		function addPost($title,$date,$text){
+			$stmt = $dbh->prepare("INSERT INTO NEWS (title, date, text) VALUES (:title, :date, :text)");
+			$stmt->bindParam(':title', $title);
+			$stmt->bindParam(':date', $date);
+			$stmt->bindParam(':text', $text);
+			$stmt->execute();
+		}
+	}
+	$post = new Post();
+	$post->addPost('tercn','1985-05-15','sad sdak sdk asdkjaskdjksaj');
 ?>
