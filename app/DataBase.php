adafc0f9da90a22743a619f6123ff8d1406e47ff
Nikita Kutsur <nikita_kutsur@art-fox.ru>
2016-10-31 20:45:54 +0300

    October

--- app/DataBase.php
+++ app/DataBase.php
@@ -1,12 +1,14 @@
 <?php
+
 $dsn = 'mysql:host=localhost;dbname=testtask';
 $username = 'test';
 $password = 'test';
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 ); 
-
 $dbh = new PDO($dsn, $username, $password, $options);
+
+
 
 
 
--- app/PostsModel.php
+++ app/PostsModel.php
@@ -1,16 +1,36 @@
 <?php
 	require_once "DataBase.php";
-
-	class Post{
-
-		function addPost($title,$date,$text){
-			$stmt = $dbh->prepare("INSERT INTO NEWS (title, date, text) VALUES (:title, :date, :text)");
-			$stmt->bindParam(':title', $title);
-			$stmt->bindParam(':date', $date);
-			$stmt->bindParam(':text', $text);
-			$stmt->execute();
+	/*
+	function viewPost(){
+		$stmt = $dbh->query('SELECT * FROM news');
+		while ($row = $stmt->fetch()){
+			   echo $row['title'] . "<br>";
+			   echo $row['date'] . "<br>";
+			   echo $row['text'] . "<br>";
+			   echo "<br>";
 		}
 	}
-	$post = new Post();
-	$post->addPost('tercn','1985-05-15','sad sdak sdk asdkjaskdjksaj');
+	*/
+	
+
+	class modelPost{
+
+		function __construct(){
+			global $dbh;
+			$stmt = $dbh->prepare("INSERT INTO news (title,text,date) VALUES (?,?,?)");
+			$stmt->bindParam(1, $title);
+			$stmt->bindParam(2, $text);
+			$stmt->bindParam(3, $date);
+
+
+			$this->title = $title;
+			$this->text = $text;
+			$this->date - $date;
+			
+			$stmt->execute();
+		}
+		
+	}
+	
+	$post = new modelPost('text','test stete efsfs','2015-10-08');
 ?>
