bab283ef68efb6453bbe03855b7e01c157b58d9f
Nikita Kutsur <nikita_kutsur@art-fox.ru>
2016-11-03 17:03:36 +0300

    Up Func

--- app/PostsController.php
+++ app/PostsController.php
@@ -8,13 +8,13 @@ class ControllerPost{
 		// $this->data = $this->db->prepare("SELECT * FROM news")->fetch();
 		$this->model = new PostsModel();
 		$this->data = $this->model->getAll();
-		
+
 	}
 
 	function Render() {
 		extract($this->data);
 		include 'ViewPost.php';
-
+		var_dump($post);
 	}
 	
 
